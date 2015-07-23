1c371fb22008fc077c0552c3dcada2ae70981083
zavernutiy <max_zavernenkov@mail.ru>
2015-07-23 18:01:39 +0300

    Added ParseLoginUI

---
+++ app/src/main/java/com/test/max/mapslogin/DispatchLaunchActivity.java
@@ -0,0 +1,57 @@
+package com.test.max.mapslogin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseUser;
+import com.parse.ui.ParseLoginBuilder;
+
+/**
+ * Created by Max on 7/23/2015.
+ */
+public class DispatchLaunchActivity extends Activity {
+    private final static String TAG = DispatchLaunchActivity.class.getCanonicalName();
+    private ParseUser currentUser;
+
+    public DispatchLaunchActivity(){
+        // Empty public constructor
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate()");
+
+        currentUser = ParseUser.getCurrentUser();
+
+        if (currentUser == null) {
+            ParseLoginBuilder builder = new ParseLoginBuilder(this);
+            startActivityForResult(builder.build(), 0);
+        } else {
+            launchMap();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult()");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null){
+            Log.d(TAG, "Login successful");
+            launchMap();
+        }
+    }
+
+    private void launchMap(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/test/max/mapslogin/MapsApplication.java
+++ app/src/main/java/com/test/max/mapslogin/MapsApplication.java
@@ -1,6 +1,7 @@
 package com.test.max.mapslogin;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
@@ -10,14 +11,18 @@ import com.parse.ParseObject;
  * Created by Max on 7/23/2015.
  */
 public class MapsApplication extends Application {
+
+    private final static String TAG = MapsApplication.class.getCanonicalName();
+
     @Override
     public void onCreate() {
+        Log.d(TAG, "onCreate()");
         super.onCreate();
 
         ParseObject.registerSubclass(User.class);
-        Parse.initialize(this, "7tfLj9j30P3Ldq4GMDBYy4lzyJkBTBCui6MtMr88", "vw41Pbx5ODstT8db6UhlE7rlbgu1OEhE4wt1KqSW");
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        
+
     }
 }
